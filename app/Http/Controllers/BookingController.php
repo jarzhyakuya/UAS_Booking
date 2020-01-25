010c248851a2b5395c97a1afd5dca54654cdd224
fajar.muchamad12@gmail.com <fajar.muchamad12@gmail.com>
2020-01-25 17:55:41 +0700

    add controller booking,pembayaran

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,73 @@
+<?php
+    
+    namespace App\Http\Controllers;
+    
+    use App\Models\Booking;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Auth;
+
+    class BookingController extends Controller
+    {
+        public function store(Request $request)
+        {
+            $input = $request->all();
+            $this->validate($request, [
+                'booking_id' => 'required|interger',
+                'meja_id' => 'required|integer',
+                'tarif_id' => 'required|integer',
+                'user_id' => 'required|integer',
+                'tanggal_booking' => 'required|date',
+                'status' => 'required|in:done,cancel,waiting'
+            ]);
+            $booking = Tarif::create($input);
+            return response()->json($booking, 200);
+        }
+        
+        public function index()
+        {
+            $booking = Booking::with('meja','tarif')->OrderBy("id", "DESC")->paginate(10)->toArray();
+            $response = [
+                "total_count" => $booking["total"],
+                "limit" => $booking["per_page"],
+                "pagination" =>[
+                    "next_page" => $booking["next_page_url"],
+                    "current_page" => $booking["current_page"]
+                ],
+                "data" => $booking["data"],
+            ];
+            return response()->json($booking, 200);
+        }
+
+        public function show($id)
+        {
+            $booking = Booking::with('meja','tarif')->find($id);
+            if(!$booking){
+                abort(404);
+            }
+            return response()->json($booking, 200);
+        }
+        
+        public function destroy($id)
+        {
+            $booking = Booking::find($id);
+            $booking->delete();
+            $message = ['message' => 'delete sucessfull', 'id' => $id ];
+            return response()->json($message, 200);
+        }
+
+        public function update(Request $request, $id)
+        {
+            $input = $request->all();
+            $booking = Booking::find($id);
+            if (!$booking) {
+                abort(404);
+            }
+            $this->validate($request, [
+                
+            ]);
+            $booking->fill($input);
+            $booking->save();
+            return response()->json($booking, 200);
+        }
+    }
+?>
---
+++ app/Http/Controllers/PembayaranController.php
@@ -0,0 +1,54 @@
+<?php
+    
+    namespace App\Http\Controllers;
+    
+    use App\Models\Pembayaran;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Auth;
+
+    class PembayaranController extends Controller
+    {
+        public function store(Request $request)
+        {
+            $input = $request->all();
+            $this->validate($request, [
+                'total' => 'required|interger',
+                'booking_id' => 'required|integer'
+            ]);
+            $pembayaran = Tarif::create($input);
+            return response()->json($pembayaran, 200);
+        }
+        
+        public function index()
+        {
+            $pembayaran = Pembayaran::with('booking')->OrderBy("id", "DESC")->paginate(10)->toArray();
+            $response = [
+                "total_count" => $pembayaran["total"],
+                "limit" => $pembayaran["per_page"],
+                "pagination" =>[
+                    "next_page" => $pembayaran["next_page_url"],
+                    "current_page" => $pembayaran["current_page"]
+                ],
+                "data" => $pembayaran["data"],
+            ];
+            return response()->json($pembayaran, 200);
+        }
+
+        public function show($id)
+        {
+            $pembayaran = Pembayaran::with('booking')->find($id);
+            if(!$pembayaran){
+                abort(404);
+            }
+            return response()->json($pembayaran, 200);
+        }
+        
+        public function destroy($id)
+        {
+            $pembayaran = Pembayaran::find($id);
+            $pembayaran->delete();
+            $message = ['message' => 'delete sucessfull', 'id' => $id ];
+            return response()->json($message, 200);
+        }
+    }
+?>
